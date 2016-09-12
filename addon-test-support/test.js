f7ffd338e3e53a708f99196490a7f66a560531b4
Chris J. Shull <cshull@apple.com>
2016-09-12 14:55:14 -0700

    handle assert.async() too

--- addon-test-support/test.js
+++ addon-test-support/test.js
@@ -19,8 +19,11 @@ sinon.config = {
   useFakeServer: false
 };
 
+let ALREADY_FAILED = {};
+
 export default function test(testName, callback) {
 
+  let sandbox;
   let wrapper = function () {
     let context = this;
     if (Ember.isBlank(context)) {
@@ -29,21 +32,51 @@ export default function test(testName, callback) {
 
     let config = sinon.getConfig(sinon.config);
     config.injectInto = context;
-    let sandbox = sinon.sandbox.create(config);
+    sandbox = sinon.sandbox.create(config);
+
+    // There are two ways to have an async test:
+    // 1. return a thenable
+    // 2. call `assert.async()`
 
     let result = callback.apply(context, arguments);
-    if (result && result.then) {
-      return result.then(data => {
-        sandbox.verifyAndRestore();
-        return data;
-      }, data => {
-        sandbox.restore();
-        return Promise.reject(data);
-      });
-    }
+    let currentTest = QUnit.config.current;
 
-    sandbox.verifyAndRestore();
-    return result;
+    // Normalize into a promise, even if the test was originally
+    // synchronous. And wait for a thenable `result` to finish first
+    // (otherwise an asynchronously invoked `assert.async()` will be
+    // ignored).
+    let promise = Ember.RSVP.resolve(result).then(data => {
+      // When `assert.async()` is called, the best way found to
+      // detect completion (so far) is to poll the semaphore. :(
+      let poll = (resolve, reject) => {
+        // Afford for the fact that we are returning a promise, which
+        // bumps the semaphore to at least 1. So when it drops to 1
+        // then everything else is complete.
+        // (0 means it already failed, e.g. by timing out.)
+        if (currentTest.semaphore <= 1) {
+          if (!currentTest.semaphore) {
+            reject(ALREADY_FAILED);
+          } else {
+            resolve(data);
+          }
+        } else {
+          setTimeout(poll, 10, resolve, reject);
+        }
+      };
+
+      return new Ember.RSVP.Promise(poll);
+    });
+
+    // todo: break into two polls: for zero, and one
+
+    return promise.then(data => {
+      sandbox.verifyAndRestore();
+      return data;
+    }, error => {
+      sandbox.restore();
+      if (error === ALREADY_FAILED) return;
+      return Ember.RSVP.reject(error);
+    });
   };
 
   try {
--- tests/unit/ember-sinon-qunit-test.js
+++ tests/unit/ember-sinon-qunit-test.js
@@ -2,10 +2,22 @@ import { module, skip } from 'qunit';
 import { isPresent } from 'ember-utils';
 import test from 'ember-sinon-qunit/test-support/test';
 import assertSinonInTestContext from '../helpers/assert-sinon-in-test-context';
+import Ember from 'ember';
 
+let origMethod;
+let obj;
 module('Unit | ember-sinon-qunit', {
   beforeEach() {
     this.foo = 'foo';
+
+    origMethod = () => {};
+    obj = {
+      method: origMethod
+    };
+  },
+
+  afterEach(assert) {
+    assert.equal(obj.method, origMethod, 'stub was reset');
   }
 });
 
@@ -17,3 +29,38 @@ assertSinonInTestContext(test);
 skip('does not destroy context from beforeEach', function (assert) {
   assert.ok(isPresent(this.foo), 'this.foo exists');
 });
+
+test('async with assert.async()', function (assert) {
+  assert.expect(2);
+  this.stub(obj, 'method');
+
+  const done = assert.async();
+  setTimeout(() => {
+    assert.notEqual(obj.method, origMethod, 'stub not reset yet');
+    done();
+  });
+});
+
+test('async with Promise', function (assert) {
+  assert.expect(2);
+  this.stub(obj, 'method');
+
+  return Ember.RSVP.resolve().then(() => {
+    assert.notEqual(obj.method, origMethod);
+  });
+});
+
+test('async with Promise and assert.async()', function (assert) {
+  assert.expect(3);
+  this.stub(obj, 'method');
+
+  return Ember.RSVP.resolve().then(() => {
+    assert.notEqual(obj.method, origMethod, 'stub not reset yet');
+    const done = assert.async();
+    setTimeout(() => {
+      assert.notEqual(obj.method, origMethod, 'stub not reset yet');
+      done();
+    });
+  });
+});
+
