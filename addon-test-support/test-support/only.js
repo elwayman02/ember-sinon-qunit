0d29526be5e52566524a7f4a71aee89a433d0f96
Jordan Hawker <hawker.jordan@gmail.com>
2019-09-03 16:47:12 -0700

    Create direct import for `ember-sinon-qunit`

---
+++ addon-test-support/test-support/only.js
@@ -0,0 +1,2 @@
+import only from '../only';
+export default only;
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  treeForAddonTestSupport(tree) {
+    // intentionally not calling _super here
+    // so that can have our `import`'s be
+    // import { ... } from 'ember-sinon-qunit';
+
+    return this.preprocessJs(tree, '/', this.name, {
+      registry: this.registry,
+    });
+  },
 };
