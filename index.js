8be11c574acbee5ff16aef2ec1fd0ffbb49e8b49
Jordan Hawker <hawker.jordan@gmail.com>
2019-09-05 13:29:32 -0700

    Add broccoli funnel for namespacing

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ module.exports = {
     // so that can have our `import`'s be
     // import { ... } from 'ember-sinon-qunit';
 
+    const Funnel = require('broccoli-funnel');
+
+    let namespacedTree = new Funnel(tree, {
+      srcDir: '/',
+      destDir: `/${this.moduleName()}`,
+      annotation: `Addon#treeForTestSupport (${this.name})`,
+    });
+
     return this.preprocessJs(tree, '/', this.name, {
       registry: this.registry,
     });
