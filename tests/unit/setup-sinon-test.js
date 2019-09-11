027536e77d893a270891f48fcede59458dc062ab
Devin Weaver <suki@tritarget.org>
2019-09-11 14:21:57 -0400

    Add tests for sinon.assert QUnit integration

--- tests/unit/setup-sinon-test.js
+++ tests/unit/setup-sinon-test.js
@@ -1,11 +1,30 @@
 import { module, test } from 'qunit';
 import setupSinon from 'ember-sinon-qunit';
+import sinon from 'sinon';
 import {
   createSandbox,
   restoreSandbox,
 } from 'ember-sinon-qunit/test-support/sinon-sandbox';
 
 module('Unit | ember-sinon-qunit | Setup in testStart/testDone', function() {
+
+  test(`configures Sinon's QUnit assertion configuration`, function(assert) {
+    assert.expect(3);
+
+    let testSpy = sinon.spy();
+    let qunit = {
+      testStart() { },
+      testDone() { },
+    };
+
+    setupSinon(qunit);
+
+    sinon.assert.notCalled(testSpy);
+    testSpy();
+    sinon.assert.calledOnce(testSpy);
+    assert.ok(true, 'expected sinon.assert to not throw');
+  });
+
   test(`configuring setup/restore`, function(assert) {
     assert.expect(4);
 
