c9af83687a9beea7beec2967d4f22178e24ebf83
Simon Ihmig <simon.ihmig@gmail.com>
2023-01-04 16:57:41 +0100

    Use ember-auto-import instead of ember-sinon

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,11 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
-    // Add options here
+    autoImport: {
+      alias: {
+        sinon: 'sinon/pkg/sinon-esm',
+      },
+    },
   });
 
   /*
