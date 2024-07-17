08c860ca4d669226d0dea8a14bdc4d82122c5809
Jordan Hawker <hawker.jordan@gmail.com>
2024-07-16 17:42:43 -0700

    Blueprint Updates: Drop Node <18, Ember <4.12

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -8,39 +8,18 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.28',
-        npm: {
-          devDependencies: {
-            '@ember/test-helpers': '^2.9.3',
-            'ember-cli': '~3.28.0',
-            'ember-data': '~3.28.0',
-            'ember-qunit': '^6.0.0',
-            'ember-source': '~3.28.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-4.4',
-        npm: {
-          devDependencies: {
-            'ember-data': '~4.4.0',
-            'ember-source': '~4.4.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-4.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.8.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-4.12',
         npm: {
           devDependencies: {
             'ember-source': '~4.12.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-5.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.4.0',
           },
         },
       },
